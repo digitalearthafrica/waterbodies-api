ab14e1167c4957195e2f8ee63e5778a5d2e22d5a
Lachlan Hurst <lachlan.hurst@gmail.com>
2024-04-03 10:43:04 +1100

    added handler that returns some time series data from the DB as CSV

--- server/app/main.py
+++ server/app/main.py
@@ -1,26 +1,27 @@
 from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from fastapi.responses import StreamingResponse
 from geojson_pydantic import Feature
+from pydantic import BaseModel
+from typing import AsyncGenerator
 
 from app.db import lifespan
 
 
 app = FastAPI(lifespan=lifespan)
 
-origins = [
-    "*"
-]
-
+# Allow all origins, and all methods
+# Without this CORS may block other systems attempting to
+# access these web services
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
+# defines structure of data returned by waterbody metadata handler
 class Waterbody(BaseModel):
     uid: str
     wb_id: int
@@ -47,6 +48,47 @@ async def get_waterbody(wb_id: int, request: Request) -> Waterbody:
                 )
             uid, wb_id, area_m2 = waterbody
             return Waterbody(uid=uid, wb_id=wb_id, area_m2=area_m2)
+
+
+async def query_waterbody_observations(
+        request: Request, wb_id: int
+    ) -> AsyncGenerator[str, None]:
+    """ Async generator that yields a string (formatted as a CSV line) for each
+    row returned by the SQL query as the query is being run.
+    """
+    # TODO - updated this query to something useful
+    query = (
+        "SELECT date, px_wet "
+        "FROM waterbody_observations AS wo "
+        "JOIN waterbodies_historical_extent AS whe "
+        "    ON wo.uid = whe.uid "
+        f"WHERE wb_id={wb_id}"
+    )
+    async with request.app.async_pool.connection() as conn:
+        async with conn.cursor() as cursor:
+            async for wb_observation in cursor.stream(query):
+                # TODO - any changes to the query above need to be reflected
+                # here
+                obs_date, obs_px_wet = wb_observation
+                csv_line = f"{str(obs_date)},{obs_px_wet}\n"
+                yield csv_line
+
+
+@app.get("/waterbody/{wb_id}/observations/csv")
+async def get_waterbody_observations_csv(
+        wb_id: int,
+        request: Request
+    ) -> StreamingResponse:
+    """
+    Returns the water body observations over time in a CSV format
+    """
+    # Stream the reponse data, this means we don't need to keep a full copy
+    # of the water observations in memeory, and we can start writing the
+    # response as soon as the first row is read from the DB
+    return StreamingResponse(
+        query_waterbody_observations(request, wb_id),
+        media_type='text/csv'
+    )
 
 
 @app.get("/waterbody/{wb_id}/geometry")
