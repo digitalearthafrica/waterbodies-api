a0b1f4bfb15836e51541319894e1d67f11d2ac14
Lachlan Hurst <lachlan.hurst@gmail.com>
2024-04-02 16:38:15 +1100

    disable CORS restrictions
    terria requests data directly from a server (and not via a proxy), so we'll need to have CORS open to support this. If needed we could lock down the origins.

--- server/app/main.py
+++ server/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from geojson_pydantic import Feature
 
@@ -6,6 +7,18 @@ from app.db import lifespan
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Waterbody(BaseModel):
