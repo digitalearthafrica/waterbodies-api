c2d867fd544b7535f9912a94604f6326d6e09d4e
Bhaskar_Pandranki <bhaskar1994p@gmail.com>
2024-05-15 13:29:48 +0530

    Remove space

--- test.py
+++ test.py
@@ -5,5 +5,3 @@ url = "http://localhost:8080/waterbody/53329/observations/csv"
 with requests.get(url, stream=True) as r:
     for chunk in r.iter_content(512):  # or, for line in r.iter_lines():
         print(chunk)
-
-
